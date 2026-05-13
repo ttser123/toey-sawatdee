b37ff33c79ecdadf6f9bbe5755e01473c41edceb
ttser123 <ttser123@gmail.com>
2026-05-13 19:08:52 +0700

    feat: initialize terraform state and add userdata configuration script

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -4,8 +4,3 @@ dnf install -y docker
 systemctl enable docker
 systemctl start docker
 
-docker run -d \
-  -p 80:3000 \
-  --name toey-sawatdee \
-  --restart unless-stopped \
-  ghcr.io/ttser123/toey-sawatdee:latest
